f3f212df5b997a5985224d8191cc4dc1966bc50f
Hammad Rashid (Hammad) <76726810+Hammad1007@users.noreply.github.com>
2022-02-12 11:37:41 +0500

    created file

---
+++ file.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <pthread.h>  // threading kai liye
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fstream>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cmath>
